7f26e74857ca4d6dbeeccad4a236f893e444ec33
Yannick Versley <versley@sfs.uni-tuebingen.de>
2010-10-29 14:43:54 +0200

    point tasks template to actual JQuery file

--- classify_argspan.py
+++ classify_argspan.py
@@ -1,16 +1,29 @@
+import sys
+sys.path.append('/home/yannickv/proj/pytree')
+
 import json
-from pytree import export
+from pytree import export, deps
+from pynlp.en import ptb_heads
 from annodb import database
 from getopt import getopt
 from array import array
+from dist_sim.fcomb import FCombo
+from ml_utils import mkdata
+from java_if import make_sd
+import morpha
+import numpy
+import traceback
 import re
 import sys
+import me_opt_new as me_opt
 
 ptb=database.get_corpus('PTB')
 annos=ptb.db.annotation
 words=ptb.words
 sents=ptb.sentences
 text_id=ptb.corpus.attribute('text_id','s')
+
+deps_extractor=deps.SimpleDepExtractor(ptb_heads.semhead_table)
 
 normalize_connector=set(['after','before','because','when','until','since','if'])
 
@@ -189,6 +202,54 @@ def make_path(n1,n2):
     lst_down.reverse()
     return ([n.cat for n in lst_up],[n.cat for n in lst_down])
 
+def pivot_features(n0,n1,n2,feat):
+    lst_up_1=[]
+    lst_up_2=[]
+    n=n1
+    while n:
+        lst_up_1.append(n)
+        n=n.parent
+    n=n2
+    while n:
+        lst_up_2.append(n)
+        n=n.parent
+    set_1=set(lst_up_1)
+    set_2=set(lst_up_2)
+    n=n0
+    set_up_0=set()
+    while n and n not in set_1 and n not in set_2:
+        set_up_0.add(n)
+        n=n.parent
+    pos1=-1
+    pos2=-1
+    pos0=-1
+    for i,n_chld in enumerate(n.children):
+        if n_chld in set_up_0:
+            pos0=i
+        if n_chld in set_1:
+            pos1=i
+        if n_chld in set_2:
+            pos2=i
+    syms=[n.cat]+[n_chld.cat for n_chld in n.children]
+    syms[pos0+1]+='*C'
+    syms[pos1+1]+='*1'
+    syms[pos2+1]+='*2'
+    feat.append('pvA='+'-'.join(syms))
+    if pos1==-1:
+        feat.append('pv1=P')
+    else:
+        if pos1<pos0:
+            feat.append('pv1=L')
+        else:
+            feat.append('pv1=R')
+    if pos2==-1:
+        feat.append('pv2=P')
+    else:
+        if pos2<pos0:
+            feat.append('pv2=L')
+        else:
+            feat.append('pv2=R')
+
 def find_modals(n,md,want_word):
     for chld in n.children:
         if chld.isTerminal():
@@ -211,7 +272,8 @@ class ConnInfo(object):
     def set_from_anno(self,anno):
         spans=anno['conn_parts']
         self.spans=spans
-        self.ws=[tuple([w.lower() for w in words[span[0]:span[1]]]) for span in spans]
+        ws=[tuple([w.lower() for w in words[span[0]:span[1]]]) for span in spans]
+        self.ws=ws
         if len(ws)==1 and ws[0][-1] in normalize_connector:
             ws=((ws[0][-1],),)
             spans=[[spans[-1][1]-1,spans[-1][1]]]
@@ -220,7 +282,7 @@ class ConnInfo(object):
         argspan1=anno['arg1']
         while left_ignore_re.match(words[argspan1[0]]):
             argspan1[0]+=1
-        while right_ignore_re.match(words[argspan1[1]]):
+        while right_ignore_re.match(words[argspan1[1]-1]):
             argspan1[1]-=1
         argspan1[0]-=s_start
         argspan1[1]-=s_start
@@ -228,134 +290,265 @@ class ConnInfo(object):
         argspan2=anno['arg2']
         while left_ignore_re.match(words[argspan2[0]]):
             argspan2[0]+=1
-        while right_ignore_re.match(words[argspan2[1]]):
+        while right_ignore_re.match(words[argspan2[1]-1]):
             argspan2[1]-=1
         argspan2[0]-=s_start
         argspan2[1]-=s_start
         self.argspan2=argspan2
-        t_json=ptb.get_parses(sent_no)['release']
-        self.t=export.from_json(t_json)
+        t_json=ptb.get_parses(self.sent_no)['release']
+        t=export.from_json(t_json)
+        self.t=t
         if spans[0][0]-s_start>=len(t.terminals) or spans[0][0]-s_start<0:
+            self.kind='multi-sentence'
+            self.t=None
             return
         self.n_conn_start=t.terminals[spans[0][0]-s_start]
         self.n_conn_end=t.terminals[spans[-1][1]-s_start-1]
         if argspan1[0]<0:
+            self.kind='anaphoric'
+            self.n1=None
+        elif argspan1[1]>=len(t.terminals):
+            self.kind='cataphoric'
             self.n1=None
         else:
+            try:
+                if t.terminals[argspan1[0]].word=='that' and t.terminals[argspan1[0]].cat=='IN':
+                        argspan1[0]+=1
+                        while left_ignore_re.match(t.terminals[argspan1[0]].word):
+                            argspan1[0]+=1
+            except IndexError:
+                print argspan1, t.terminals
+                raise
             self.n1=find_node_simple(t,argspan1[0],argspan1[1]-argspan1[0])
         self.n2=find_node_simple(t,argspan2[0],argspan2[1]-argspan2[0])
-        
-            
+
+
+def pruned_candlist(n_start,paths):
+    cands=[]
+    gather_candidates_up(n_start,[],paths,cands)
+    if not cands:
+        try:
+            parent_full=n_start.parent.to_full([])
+        except KeyError:
+            parent_full=n_start.parent
+        try:
+            possible_keys=paths[0][n_start.cat][0].keys()
+        except KeyError:
+            possible_keys=None
+        print >>sys.stderr, "No cands for %s (parent=%s, feasible=%s)"%(n_start.to_full([]),parent_full,possible_keys)
+        return []
+    max_score=max([x[3] for x in cands])
+    return [x for x in cands if x[3]>=0.1*max_score]
+
+## PTB sections (start)
+##  0 ->       0
+## 10 ->  441305
+## 20 ->  956696
+## 22 -> 1004073
+## 23 -> 1044112
+## 24 -> 1140913
+## train on sec 00-09, test on 10-21
+train_criteria={'level':'pdtb','reltype':'Explicit','span':{'$lt':441305}}
+test_criteria={'level':'pdtb','reltype':'Explicit','span':{'$gte':441305,'$lt':1004073}}
 
 # Step 1: gather paths
-for anno in annos.find({'level':'pdtb','reltype':'Explicit'}):
+for anno in annos.find(train_criteria):
     ci=ConnInfo()
     try:
         ci.set_from_anno(anno)
     except:
-        print "Unexpected error:", sys.exc_info()[0]
+        traceback.print_exc()
+        break
+    if ci.t is None:
         continue
-    spans=anno['conn_parts']
-    ws=[tuple([w.lower() for w in words[span[0]:span[1]]]) for span in spans]
-    if len(ws)==1 and ws[0][-1] in normalize_connector:
-        ws=((ws[0][-1],),)
-        spans=[[spans[-1][1]-1,spans[-1][1]]]
-    sent_no=sents.cpos2struc(spans[-1][0])
-    s_start,s_end=sents[sent_no][:2]
-    argspan1=anno['arg1']
-    while left_ignore_re.match(words[argspan1[0]]):
-        argspan1[0]+=1
-    while right_ignore_re.match(words[argspan1[1]]):
-        argspan1[1]-=1
-    argspan1[0]-=s_start
-    argspan1[1]-=s_start
-    argspan2=anno['arg2']
-    while left_ignore_re.match(words[argspan2[0]]):
-        argspan2[0]+=1
-    while right_ignore_re.match(words[argspan2[1]]):
-        argspan2[1]-=1
-    argspan2[0]-=s_start
-    argspan2[1]-=s_start
-    try:
-        t_json=ptb.get_parses(sent_no)['release']
-    except KeyError:
-        print "No parse for sentence %d"%(sent_no,)
-    t=export.from_json(t_json)
-    if spans[0][0]-s_start>=len(t.terminals) or spans[0][0]-s_start<0:
-        print >>sys.stderr, "multi-sentence connective: %s"%(ws,)
-        continue
-    n_conn_start=t.terminals[spans[0][0]-s_start]
-    n_conn_end=t.terminals[spans[-1][1]-s_start-1]
-    #print words[s_start:s_end+1],ws,n_conn_start.to_full([]),n_conn_end.to_full([])
-    if argspan1[0]<0:
-        pass
-        #print 'arg1=*anaphoric*'
-    else:
-        n1=find_node_simple(t,argspan1[0],argspan1[1]-argspan1[0])
-        #print 'arg1',n1.to_full([])
-        path1=make_path(n_conn_start,n1)
-        #print path1
+    if ci.n1 is not None:
+        path1=make_path(ci.n_conn_start,ci.n1)
         enter_path(path1[0],path1[1],all_paths_arg1)
-    n2=find_node_simple(t,argspan2[0],argspan2[1]-argspan2[0])
-    #print 'arg2',n2.to_full([])
-    path2=make_path(n_conn_end,n2)
-    #print path2
+    path2=make_path(ci.n_conn_end,ci.n2)
     enter_path(path2[0],path2[1],all_paths_arg2)
 
-# Step 2: determine path candidates
-for anno in annos.find({'level':'pdtb','reltype':'Explicit'}):
-    spans=anno['conn_parts']
-    ws=[tuple([w.lower() for w in words[span[0]:span[1]]]) for span in spans]
-    if len(ws)==1 and ws[0][-1] in normalize_connector:
-        ws=((ws[0][-1],),)
-        spans=[[spans[-1][1]-1,spans[-1][1]]]
-    sent_no=sents.cpos2struc(spans[-1][0])
-    s_start,s_end=sents[sent_no][:2]
-    argspan1=anno['arg1']
-    while left_ignore_re.match(words[argspan1[0]]):
-        argspan1[0]+=1
-    while right_ignore_re.match(words[argspan1[1]-1]):
-        argspan1[1]-=1
-    argspan1[0]-=s_start
-    argspan1[1]-=s_start
-    argspan2=anno['arg2']
-    while left_ignore_re.match(words[argspan2[0]]):
-        argspan2[0]+=1
-    while right_ignore_re.match(words[argspan2[1]-1]):
-        argspan2[1]-=1
-    argspan2[0]-=s_start
-    argspan2[1]-=s_start
+def anaphoric_features(ci,feats):
+    feats.append("C"+'+'.join(['_'.join(ws) for ws in ci.ws]))
+    feats.append("W1"+ci.n_conn_start.word)
+    feats.append("P1"+ci.n_conn_start.cat)
+
+def arg2only_features(ci,c,feats):
+    feats.append("C"+'+'.join(['_'.join(ws) for ws in ci.ws]))
+    feats.append("W1"+ci.n_conn_start.word)
+    feats.append("P1"+ci.n_conn_start.cat)
+    n2=c[2]
+    path2=make_path(ci.n_conn_end,n2)
+    feats.append("p2%s+%s"%('-'.join(path2[0]),'-'.join(path2[1])))
+
+def both_features(ci,c1,c2,feats):
+    feats.append("C"+'+'.join(['_'.join(ws) for ws in ci.ws]))
+    feats.append("W1"+ci.n_conn_start.word)
+    feats.append("P1"+ci.n_conn_start.cat)
+    n1=c1[2]
+    n2=c2[2]
+    path1=make_path(ci.n_conn_start,n1)
+    feats.append("p2%s+%s"%('-'.join(path1[0]),'-'.join(path1[1])))
+    path2=make_path(ci.n_conn_end,n2)
+    feats.append("p2%s+%s"%('-'.join(path2[0]),'-'.join(path2[1])))
+    pivot_features(ci.n_conn_end,n1,n2,feats)
+
+
+# Step 2: determine path candidates and train classifier/rankers
+fc_anaphoric=FCombo(2,bias_item='**BIAS**')
+data_anaphoric=[]
+fc_arg2only=FCombo(2)
+data_arg2only=[]
+fc_both=FCombo(2)
+data_both=[]
+for anno in annos.find(train_criteria):
+    ci=ConnInfo()
     try:
-        t_json=ptb.get_parses(sent_no)['release']
-    except KeyError:
-        print "No parse for sentence %d"%(sent_no,)
-    t=export.from_json(t_json)
-    if spans[0][0]-s_start>=len(t.terminals) or spans[0][0]-s_start<0:
-        print >>sys.stderr, "multi-sentence connective: %s"%(ws,)
+        ci.set_from_anno(anno)
+    except:
+        traceback.print_exc()
+        break
+    if ci.t is None:
         continue
-    n_conn_start=t.terminals[spans[0][0]-s_start]
-    n_conn_end=t.terminals[spans[-1][1]-s_start-1]
-    print words[s_start:s_end+1],ws,n_conn_start.to_full([]),n_conn_end.to_full([])
-    if argspan1[0]<0:
-        print 'arg1=*anaphoric*'
+    t=ci.t
+    deps_extractor(t)
+    morpha.lemmatize(t)
+    cands2=pruned_candlist(ci.n_conn_end,all_paths_arg2)
+    if ci.n1 is None:
+        anaphoric_val=False
+        ## create arg2only examples
+        good=[]
+        bad=[]
+        for c in cands2:
+            feat=[]
+            arg2only_features(ci,c,feat)
+            fval=fc_arg2only(mkdata(feat))
+            if ci.n2==c[2]:
+                good.append(fval)
+            else:
+                bad.append(fval)
+        if good and bad:
+            data_arg2only.append([good,bad])
     else:
-        n1=find_node_simple(t,argspan1[0],argspan1[1]-argspan1[0])
-        print 'arg1',[n.word for n in t.terminals[argspan1[0]:argspan1[1]]],n1.to_full([])
-        path1=make_path(n_conn_start,n1)
-        print path1
-        cands=[]
-        gather_candidates_up(n_conn_start,[],all_paths_arg1,cands)
-        max_score=max([x[3] for x in cands])
-        cands=[x for x in cands if x[3]>=0.1*max_score]
-        found=False
-        for c in cands:
-            print "cand[1]:",c
-            if c[2]==n1:
-                found=True
-        if not found:
-            print "** Arg1 not covered"
-    n2=find_node_simple(t,argspan2[0],argspan2[1]-argspan2[0])
-    print 'arg2',n2.to_full([])
-    path2=make_path(n_conn_end,n2)
-    print path2
-    enter_path(path2[0],path2[1],all_paths_arg2)
+        anaphoric_val=True
+        ## create _both examples
+        n1=ci.n1
+        cands1=pruned_candlist(ci.n_conn_start,all_paths_arg1)
+        good=[]
+        bad=[]
+        for c1 in cands1:
+            for c2 in cands2:
+                ## create arg_both examples
+                feat=[]
+                both_features(ci,c1,c2,feat)
+                fval=fc_both(mkdata(feat))
+                if ci.n1==c1[2] and ci.n2==c2[2]:
+                    good.append(fval)
+                else:
+                    bad.append(fval)
+        if good and bad:
+            data_both.append([good,bad])
+    feat=[]
+    anaphoric_features(ci,feat)
+    data_anaphoric.append((fc_anaphoric(mkdata(feat)),anaphoric_val))
+
+## Step 2b: create classifiers/rankers
+fc_arg2only.dict.growing=False
+fc_both.dict.growing=False
+fc_anaphoric.dict.growing=False
+weights_arg2only=me_opt.train_me_sparse(data_arg2only,fc_arg2only.dict)
+weights_both=me_opt.train_me_sparse(data_both,fc_both.dict)
+x=numpy.zeros(len(fc_anaphoric.dict),'d')
+iflag,n_iter,x,d1=me_opt.run_lbfgs(x,me_opt.sparse_unary_func,(data_anaphoric,))
+weights_anaphoric=x
+
+## Step 3: evaluate everything
+arg1_total=0
+arg1_exact=0
+arg1_head=0
+
+arg2_total=0
+arg2_exact=0
+arg2_head=0
+
+anaphoric_eval=numpy.zeros([2,2])
+
+for anno in annos.find(test_criteria):
+    ci=ConnInfo()
+    try:
+        ci.set_from_anno(anno)
+    except:
+        traceback.print_exc()
+        break
+    if ci.t is None:
+        continue
+    t=ci.t
+    deps_extractor(t)
+    morpha.lemmatize(t)
+    cands2=pruned_candlist(ci.n_conn_end,all_paths_arg2)
+    if ci.n1 is None:
+        anaphoric_val=False
+        ## create arg2only examples
+        best=None
+        best_score=-1000
+        for c in cands2:
+            feat=[]
+            arg2only_features(ci,c,feat)
+            fval=fc_arg2only(mkdata(feat))
+            score=fval.dotFull(weights_arg2only)
+            if score>best_score:
+                best=c
+                best_score=score
+        arg2_total+=1
+        if best!=None and ci.n2==best[2]:
+            arg2_exact+=1
+        if best!=None and ci.n2.head==best[2].head:
+            arg2_head+=1
+    else:
+        anaphoric_val=True
+        ## create _both examples
+        n1=ci.n1
+        cands1=pruned_candlist(ci.n_conn_start,all_paths_arg1)
+        best=(None,None)
+        best_score=-1000
+        for c1 in cands1:
+            for c2 in cands2:
+                ## create arg_both examples
+                feat=[]
+                both_features(ci,c1,c2,feat)
+                fval=fc_both(mkdata(feat))
+                score=fval.dotFull(weights_both)
+                if score>best_score:
+                    best=(c1,c2)
+                    best_score=score
+        best1,best2=best
+        arg1_total+=1
+        if best1!=None:
+            if ci.n1==best1[2]:
+                arg1_exact+=1
+            if ci.n1.head==best1[2].head:
+                arg1_head+=1
+            else:
+                print ' '.join([n.word for n in ci.t.terminals]),ci.ws
+                print "wrong Arg1: %s"%(best1[2].to_full([]))
+                print "wanted: %s"%(ci.n1.to_full([]))
+        arg2_total+=1
+        if best2!=None:
+            if ci.n2==best2[2]:
+                arg2_exact+=1
+            if ci.n2.head==best2[2].head:
+                arg2_head+=1
+    feat=[]
+    anaphoric_features(ci,feat)
+    anaphoric_decision=(fc_anaphoric(mkdata(feat)).dotFull(weights_anaphoric)>0)
+    anaphoric_eval[anaphoric_val][anaphoric_decision]+=1
+
+print "Arg1(exact): %d/%d=%.3f"%(arg1_exact,arg1_total,float(arg1_exact)/arg1_total)
+print "Arg1(head):  %d/%d=%.3f"%(arg1_head,arg1_total,float(arg1_head)/arg1_total)
+print "Arg2(exact): %d/%d=%.3f"%(arg2_exact,arg2_total,float(arg2_exact)/arg2_total)
+print "Arg2(head):  %d/%d=%.3f"%(arg2_head,arg2_total,float(arg2_head)/arg2_total)
+prec=float(anaphoric_eval[1,1])/anaphoric_eval[:,1].sum()
+recl=float(anaphoric_eval[1,1])/anaphoric_eval[1,:].sum()
+f1=2*prec*recl/(prec+recl)
+print "anaphoric: P=%d/%d=%.3f R=%d/%d=%.3f F1=%.3f"%(anaphoric_eval[1,1],anaphoric_eval[:,1].sum(),prec,
+                                                 anaphoric_eval[1,1],anaphoric_eval[1,:].sum(),recl,
+                                                 f1)
